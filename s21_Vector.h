88c6a73bed5022d4339ba0ff7ef2dab68f4716db
Sherlyn Tigra <sherlynt@ob-j7.nsk.21-school.ru>
2023-06-20 19:33:08 +0700

    r

--- s21_Vector.h
+++ s21_Vector.h
@@ -1,5 +1,6 @@
 #ifndef SRC_S21_VECTOR_H_
 #define SRC_S21_VECTOR_H
+// #include <initializer_list.h>
 
 namespace s21 {
 template <typename T>
@@ -13,17 +14,36 @@ class Vector {
   using const_iterator = const T*;
   using size_type = size_t;
 
-  Vector() : size_(0), capacity_(0){};
+  Vector() : size_(0), capacity_(0), p_vector_(NULL){};
   Vector(size_type n) : size_(n), capacity_(n) {
-    p_vector = new value_type(n);
+    p_vector_ = new value_type[n];
   };
-  vector(std::initializer_list<value_type> const& items);
+  Vector(std::initializer_list<value_type> const& items) : {
+    size_ = items.size();
+    capacity_ = items.size();
+    p_vector_ = new value_type[items.size()];
+
+    // auto item = items.begin();
+    // for (size_t i = 0; item != items.end(); item++, i++) p_vector_[i] =
+    // *item; std::cout << "begin iterator " << items.begin() << std::endl;
+  };
+
+  reference operator[](size_type pos) { return p_vector_[pos]; };
+  //   value_type operator[](int index) { return p_vector_[index]; };
+  value_type operator=(const value_type& v){
+
+  };
+
   size_t get_size() { return size_; };
+  size_t get_capacity() { return capacity_; };
+  T* get_pointer() { return p_vector_; };
+
+  //   reference operator[](size_type pos);	access specified element
 
  private:
   size_t size_;
   size_t capacity_;
-  value_type* p_vector;
+  value_type* p_vector_;
 };
 }  // namespace s21
 #endif  // SRC_S21_VECTOR_H_
--- test.cc
+++ test.cc
@@ -8,19 +8,41 @@
 
 // typename s21::Vector<T>::size_type s21::Vector<T>::get_size() { return size_;
 // }
+using std::cout;
+using std::endl;
 
 int main() {
   //
-  s21::Vector<int> vec(43);
-  std::cout << "aboba: " << vec.get_size() << std::endl;
+  s21::Vector<int> vec{1, 2, 3, 4, 5};
 
+  std::cout << "size: " << vec.get_size() << std::endl;
+  std::cout << "capacity: " << vec.get_capacity() << std::endl;
+  std::cout << "p_vector: " << vec.get_pointer() << endl;
+  // vec[2] = 33;
+  // for (int i = 0; i < 150; i++) {
+  //   cout << vec[i] << endl;
+  // }
+  // for (size_t i = 0; i < vec.get_capacity(); i++) {
+  // cout << *(vec.get_pointer()) << endl;
+
+  // << vec.get_pointer() << std::endl;
+  // cout << vec[0] << endl;
   // std::vector<int> vect;
   // std::cout << vect.capacity() << std::endl;
 
   // vec.value
   //
-  std::vector<int> obame = {1, 5, 8};
 
+  std::vector<int> obame{6, 4, 7, 9, 9};
+  cout << "capa " << obame.capacity() << endl;
+  obame.at(4) = 55;
+  cout << obame.size() << endl;
+  for (auto var : obame) {
+    cout << var << endl;
+  }
+  // cout << *obame.begin() << endl;
+  // obame{1, 2};
+  // cout << obame.at(43) << endl;
   // while (!obame.empty()) {
   //   int i = obame.size() - 1;
   //   std::cout << obame[i] << std::endl;
